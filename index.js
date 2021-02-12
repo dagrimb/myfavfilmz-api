f975f0326a1a2dcc795ae55ec41e865afa85da36
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-02-11 21:31:40 -0800

    update to reflect revisions

--- index.js
+++ index.js
@@ -70,11 +70,11 @@ app.get('/movies', (req, res) => {
 
 //GET route that returns a movie description
 app.get('/movies/:name/description', (req, res) => {
-  let movie = movie.find((movie) => 
+  let movie = movies.find((movie) => 
     { return movie.name === req.params.name });
 
   if (movie) {
-    let description = movie.description
+    let description = Object.values(movie.description);
 
     res.status(201).send('Synopsis: ' + description);
   } else {
@@ -84,7 +84,7 @@ app.get('/movies/:name/description', (req, res) => {
 
 //GET route that returns a movie genre
 app.get('/movies/:name/genre', (req, res) => {
-  let movie = movie.find((movie) =>
+  let movie = movies.find((movie) =>
     { return movie.name === req.params.name });
 
   if (movie) {
@@ -98,7 +98,7 @@ app.get('/movies/:name/genre', (req, res) => {
 
 //GET route that returns info about a movie's director
 app.get('/movies/:name/director', (req, res) => {
-  let movie = movie.find((movie) =>
+  let movie = movies.find((movie) =>
     { return movie.name === req.params.name });
   
   if (movie) {
@@ -112,7 +112,7 @@ app.get('/movies/:name/director', (req, res) => {
 
 //GET route that returns a movie's images
 app.get('/movies/:name/images', (req, res) => {
-  let movie = movie.find((movie) =>
+  let movie = movies.find((movie) =>
   { return movie.name === req.params.name });
 
 if (movie) {
@@ -137,6 +137,54 @@ app.get('/', (req, res) => {
 })
 */
 
+let users = [
+  {
+    name: 'Billy Loomis',
+    age: 51,
+    username: 'surprisesydney51',
+    favMovies: {
+    movieId: 'Scream'
+      }
+  },
+  {
+    name: 'Danny Madigan',
+    age: 40,
+    username: 'iminamovie40',
+    favMovies: {
+        movieId: 'Last Action Hero'
+      }
+  },
+  {
+    name: 'Mia Wallace',
+    age: 50,
+    username: 'dontdodrugs50',
+    favMovies: {
+        movieId: 'Pulp Fiction'
+      }
+  },
+  {
+    name: 'Leo Getz',
+    age: 78,
+    username: 'wtuneedleogetz78',
+    favMovies: {
+      movieId: 'Lethal Weapon 2'
+      }
+  },
+  {
+    name: 'Raymond Babbit',
+    age: 83,
+    username: '20mins2wapner',
+    favMovies: {
+        movieId: 'Rain Man'
+      }
+    }
+];
+
+//GET route that returns a list of ALL movies to the user
+app.get('/users', (req, res) => {
+  res.json(users);
+})
+
 //POST route that allows new users to register
 app.post('/users', (req, res) => {
   let newUser = req.body;
@@ -150,47 +198,47 @@ app.post('/users', (req, res) => {
   }
 });
 
-//PUT route that allows new ysers to update their username
+//PUT route that allows new users to update their username
 app.put('/users/:name/:username', (req, res) => {
-  let user = users.find((user) => { return users.name === req.params.name });
+  let user = users.find((user) => { return user.name === req.params.name });
     if (user) {
-      user.username[req.params.username] = parseInt(req.params.newname);
-      res.status(201).send('User' + req.params.name + ' now has the username of ' + req.params.newname);
+      res.status(201).send('User ' + req.params.name + ' now has the username of ' + req.params.username + '.');
     } else {
         res.status(404).send('User with the name ' + req.params.name + ' was not found');
     }
 });
 
 //POST route that allows users to add a movie to their list of favorites
-app.post('/users/:name/:username/:favorites', (req, res) => {
-  let newFilm = req.body;
-  if (!newFilm.name) {
+app.post('/users/:username/favMovies', (req, res) => {
+  let newFave = req.body;
+  if (!newFave.movieId) {
     const message = 'Please include the name of your film';
     res.status(400).send(message);
   } else {
-      newFilm.id = uuid.v4();
-      favorites.push(newFilm);
-      res.status(201).send(newFilm);
+      newFave.id = uuid.v4();
+      favMovies.push(newFave);
+      res.status(201).send(newFave);
   }
 });
 
 //DELETE route that allows users to remove a movie from their list of favorites
-app.delete('/users/:name/:username/:favorites', (req, res) => {
-  let favorite = favorites.find((favorite) => { return favorite.id === req.params.id });
+app.delete('/users/:username/favMovies/:movieId', (req, res) => {
+  let favorite = favMovies.find((movieId) => { return movieId === req.params.movieId });
   
   if (favorite) {
-    favorites = favorites.filter((obj) => { return obj.id !== req.params.id });
-    res.status(201).send('Movie # ' + res.params.id + ' was deleted');
+    favMovies = favMovies.filter((obj) => { return obj.movieId !== req.params.movieId });
+    res.status(201).send('Movie ' + req.params.movieId + ' was deleted');
   }
 });
 
+
 //DELETE route that allows existing user to de-register
 app.delete('/users/:name', (req, res) => {
-  let user = users.find((name) => { return name.id === req.params.id });
+  let user = users.find((user) => { return user.name === req.params.name });
   
   if (user) {
-    users = users.filter((obj) => { return obj.id !== req.params.id });
-    res.status(201).send('User # ' + res.params.id + ' was de-registered');
+    users = users.filter((obj) => { return obj.name !== req.params.name });
+    res.status(201).send('User ' + req.params.name + ' was de-registered');
   }
 });
 
