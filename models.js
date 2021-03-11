4ca0c20aa985b0cbbc9966c08975d19ff2095ebe
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-03-11 10:02:43 -0800

    create models.js

---
+++ models.js
@@ -0,0 +1,57 @@
+//importing package
+const mongoose = require('mongoose');
+
+//defining a schema for documents in the "Movies" collection
+let movieSchema = mongoose.Schema({
+    Title: {type: String, required: true},
+    Description: {
+        Synopsis: String, 
+        Description: String,
+        Source: String
+    },
+    //Genre attribute
+    Genre: {
+        Name: String,
+        Description: String,
+        Source: String
+    },
+    //Director attribute
+    Director: {
+        Name: String,
+        Bio: String,
+        Birth: String,
+        Death: String,
+        Source: String
+    },
+    //Image of poster or still image of movie
+    ImagePath: String,
+    Featured: Boolean,
+    //Actors attribute
+    Actors: [String],
+    //Score
+    Rotten_Tomatoes_score: String,
+    Year: String
+});
+
+let actorSchema = mongoose.Schema({
+     Name: {type: String, required: true},
+     Movies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]  
+});
+
+
+//defining a schema for documents in the "Users" collection
+let userSchema = mongoose.Schema({
+    Username: {type: String, required: true},
+    Password: {type: String, required: true},
+    Email: {type: String, required: true},
+    Birthday: Date,
+    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+});
+
+let Movie = mongoose.model('Movie', movieSchema);
+let Actor = mongoose.model('Actor', actorSchema);
+let User = mongoose.model('User', userSchema);
+
+module.exports.Movie = Movie;
+module.exports.Actor = Actor;
+module.exports.User = User;
