dd83f716f6a393f70907810d53c41e53a57b831f
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-03-30 13:47:15 -0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,20 @@ mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifie
 mongoose.set('useFindAndModify', false);
 
 const express = require('express')
-//const bodyParser = require('body-parser')
+const cors = require('cors');
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234/', 'http://local'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 const app = express()
 
 let auth = require('./auth')(app);
@@ -25,22 +38,12 @@ app.use((err, req, res, next) => {
     res.status(500).send('We have a problem here....');
 })
 
-const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234/', 'http://local'];
 
 
 
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+
+
 
 const { check, validationResult } = require('express-validator');
 
