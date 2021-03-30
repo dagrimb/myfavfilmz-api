1a8f71cd79ce4dcea7572ac7f3ec76b9560dffea
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-03-30 13:20:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -10,13 +10,8 @@ mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifie
 mongoose.set('useFindAndModify', false);
 
 const express = require('express')
-const bodyParser = require('body-parser')
+//const bodyParser = require('body-parser')
 
-
-
-const app = express()
-
-app.use(bodyParser.json())
 
 let auth = require('./auth')(app);
 
@@ -32,6 +27,9 @@ app.use((err, req, res, next) => {
 
 const cors = require('cors');
 let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234/', 'http://local'];
+
+const app = express()
+
 
 app.use(cors({
   origin: (origin, callback) => {
