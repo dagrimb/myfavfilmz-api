5fe7726a2a8fce61080321d83afef8b2cb805e1e
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-05-04 18:08:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,15 +9,16 @@ const Users = Models.User;
 mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('useFindAndModify', false);
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
 
 const cors = require('cors');
 
 const dotenv = require("dotenv");
 dotenv.config();
 
-const app = express()
 
 
 
