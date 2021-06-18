d2fbcdc4ed286df748f81503e5503d3359a3f9ad
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-06-18 12:47:24 -0700

    attempt to convert date format in business logic

--- models.js
+++ models.js
@@ -54,7 +54,7 @@ const userSchema = mongoose.Schema({
     Username: {type: String, required: true},
     Password: {type: String, required: true},
     Email: {type: String, required: true},
-    Birthday: Date,
+    Birthday: Date.toLocaleDateString(),
     FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
 
