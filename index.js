4d2450b41a80d6f663930d05e61747a7c59f9c03
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-06-18 13:21:20 -0700

    Update index.js

--- index.js
+++ index.js
@@ -204,7 +204,7 @@ app.post('/users',
           Username: req.body.Username,
           Password: hashedPassword,
           Email: req.body.Email,
-          Birthday: req.body.Birthday.toLocaleDateString()
+          Birthday: req.body.Birthday
         })
         //let client know if request was successful
         .then((user) => {res.status(201).json(user) })
