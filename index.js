b10db2bf900feafd5f852224bf5845b90f80a2bf
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-07-16 07:45:06 -0700

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use((err, req, res, next) => {
 
 
 let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'https://myfavfilmz.herokuapp.com', 'http://localhost:1234', 
-'https://myfavfilmz.herokuapp.com/login', 'http://myfavfilmz.herokuapp.com/movies', 'https://myfavfilmz.herokuapp.com/users', 'https://myfavfilmz.netlify.app/'];
+'https://myfavfilmz.herokuapp.com/login', 'http://myfavfilmz.herokuapp.com/movies', 'https://myfavfilmz.herokuapp.com/users', 'https://myfavfilmz.netlify.app'];
 
 app.use(cors({
   origin: (origin, callback) => {
