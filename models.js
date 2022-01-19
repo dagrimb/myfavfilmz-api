178e87acdbb80110c77afcf2967424859648dbb5
dagrimb <74441727+dagrimb@users.noreply.github.com>
2022-01-18 21:55:37 -0800

    Update models.js

--- models.js
+++ models.js
@@ -50,7 +50,6 @@ const bcrypt = require('bcrypt');
 
 //defining a schema for documents in the "Users" collection
 const userSchema = mongoose.Schema({
-    _id: {type: String, required: true},
     Username: {type: String, required: true},
     Password: {type: String, required: true},
     Email: {type: String, required: true},
